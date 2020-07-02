64cb0d240f0400827c4fb30ffef33f03fe85bd4b
law-wang <47406212+law-wang@users.noreply.github.com>
2020-07-02 11:46:41 -0700

    fixed header alignment

--- _site/assets/scripts/main.js
+++ _site/assets/scripts/main.js
@@ -3,26 +3,26 @@ function greeting() {
   var hr = day.getHours();
   if (hr == 0) { document.getElementById("note").innerHTML = "12AM: The waves come after midnight."; }
   if (hr == 1) { document.getElementById("note").innerHTML = "1AM: Let's fall asleep to revolution, then wake up next to a sad excuse"; }
-  if (hr == 2) { document.getElementById("note").innerHTML = "2AM: We got our champagne dreams, in an endless drought"; }
+  if (hr == 2) { document.getElementById("note").innerHTML = "2AM: I got all I need, in a world of doubt. We got our champagne dreams, in an endless drought"; }
   if (hr == 3) { document.getElementById("note").innerHTML = "3AM: Should we drink a little more of this Roman coke"; }
   if (hr == 4) { document.getElementById("note").innerHTML = "4AM: Wasted youth, always on the road"; }
   if (hr == 5) { document.getElementById("note").innerHTML = "5AM: Last of a dying breed"; }
-  if (hr == 6) { document.getElementById("note").innerHTML = "6AM: the lies I weave are all so intricate"; }
+  if (hr == 6) { document.getElementById("note").innerHTML = "6AM: All the cigarette smoke, can't escape a closing window"; }
   if (hr == 7) { document.getElementById("note").innerHTML = "7AM: Turn around and you’ll see what I see"; }
   if (hr == 8) { document.getElementById("note").innerHTML = "8AM: to be honest, what I liked, were the things we didn’t know"; }
   if (hr == 9) { document.getElementById("note").innerHTML = "9AM: Would you believe me now, if I say I got caught up in a wave, almost gave it away"; }
-  if (hr == 10) { document.getElementById("note").innerHTML = "10AM: Our country, guess it was a lawless land"; }
+  if (hr == 10) { document.getElementById("note").innerHTML = "10AM: But I'm still not sure if fear's a rival or close relative to truth"; }
   if (hr == 11) { document.getElementById("note").innerHTML = "11AM: Hang your head low in the glow of the vending machine I'm not dying"; }
-  if (hr == 12) { document.getElementById("note").innerHTML = "12PM: But there's no going back against this california feeling"; }
+  if (hr == 12) { document.getElementById("note").innerHTML = "12PM: There's no going back against this california feeling"; }
   if (hr == 13) { document.getElementById("note").innerHTML = "1PM: I guess you win some and lose some, 'long as the outcome's income. I want it all, and then some."; }
   if (hr == 14) { document.getElementById("note").innerHTML = "2PM: It’s a train wreck, but I won’t crash it"; }
-  if (hr == 15) { document.getElementById("note").innerHTML = "3PM: tell me all your original sins"; }
+  if (hr == 15) { document.getElementById("note").innerHTML = "3PM: But here we are, after all the messes and confessions, to the stars"; }
   if (hr == 16) { document.getElementById("note").innerHTML = "4PM: I’ll start somewhere new, and let you have LA"; }
-  if (hr == 17) { document.getElementById("note").innerHTML = "5PM: All the cigarette smoke, can't escape a closing window"; }
+  if (hr == 17) { document.getElementById("note").innerHTML = "5PM: Let’s pick the truth we believe in, like a bad religion, tell me all your original sins"; }
   if (hr == 18) { document.getElementById("note").innerHTML = "6PM: Have you thought it over, have you gotten what you deserved"; }
   if (hr == 19) { document.getElementById("note").innerHTML = "7PM: You are what you love not who loves you"; }
   if (hr == 20) { document.getElementById("note").innerHTML = "8PM: clear blue water, high tide, came and brought you in"; }
-  if (hr == 21) { document.getElementById("note").innerHTML = "9PM: We're walking the wire, love."; }
+  if (hr == 21) { document.getElementById("note").innerHTML = "9PM: 'Cause you know the good die young, but so did this, and so it must be better than I think it is"; }
   if (hr == 22) { document.getElementById("note").innerHTML = "10PM: Try to shine. Stay wild."; }
   if (hr == 23) { document.getElementById("note").innerHTML = "11PM: When it rains it pours, so stay thirsty like before"; }
 }
